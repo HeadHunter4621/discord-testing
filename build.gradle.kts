d08e6df86510d996ecba2946c8bd568f98a8d952
Kona <81715373+HeadHunter4621@users.noreply.github.com>
2024-06-28 10:12:06 -0700

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    application
 }
 
 group = "org.example"
@@ -11,6 +12,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("net.dv8tion:JDA:5.0.0-alpha.14")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.0")
 }
 
 tasks.test {
@@ -19,3 +22,9 @@ tasks.test {
 kotlin {
     jvmToolchain(21)
 }
+/*
+application {
+    // This syntax has been updated to use the correct way to specify the main class in the latest versions of Gradle.
+    mainClass.set("MainKt")
+}
+*/
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,24 @@
-package org.example
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 fun main() {
-    println("Hello World!")
+    val token = "YOUR_DISCORD_BOT_TOKEN"
+
+    val jda = JDABuilder.createDefault(token)
+        .setActivity(Activity.playing("Kotlin Bot"))
+        .addEventListeners(MessageListener())
+        .build()
+}
+
+class MessageListener : ListenerAdapter() {
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        if (event.author.isBot) return
+
+        val message = event.message.contentRaw
+        if (message == "!ping") {
+            event.channel.sendMessage("Pong!").queue()
+        }
+    }
 }
