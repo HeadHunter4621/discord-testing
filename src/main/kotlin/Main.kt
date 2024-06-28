d04f1045d0fc1e6602b5d8f1e3500c18e74b741c
Kona <81715373+HeadHunter4621@users.noreply.github.com>
2024-06-28 11:18:40 -0700

    AAAAA IT WORKS I just had to toggle "MESSAGE CONTENT INTENT" in Discord's developer portal bot settings to on.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,22 +2,29 @@ import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.Commands
+import net.dv8tion.jda.api.requests.GatewayIntent
 import java.io.File
+
 
 fun main() {
     val token: String = File("token.txt").readText()
 
     val jda = JDABuilder.createDefault(token)
-        .setActivity(Activity.playing("Kotlin Bot"))
+        .enableIntents(GatewayIntent.MESSAGE_CONTENT) // Enable MESSAGE_CONTENT intent
+        .setActivity(Activity.playing("Ping Pong"))
         .addEventListeners(MessageListener())
         .build()
 
     jda.awaitReady()
     println("Bot is ready!")
+
 }
 
 class MessageListener : ListenerAdapter() {
     override fun onMessageReceived(event: MessageReceivedEvent) {
+        val guild = event.guild
         println("Recieved a message: ${event.message.contentRaw}")
         if (event.author.isBot) return
 
@@ -25,6 +32,13 @@ class MessageListener : ListenerAdapter() {
         if (message == "?ping") {
             println("Recieved ping command")
             event.channel.sendMessage("Pong!").queue()
+
+            guild.updateCommands().addCommands(
+                Commands.slash("echo", "Repeats messages back to you.")
+                    .addOption(OptionType.STRING, "message", "The message to repeat.")
+                    .addOption(OptionType.INTEGER, "times", "The number of times to repeat the message.")
+                    .addOption(OptionType.BOOLEAN, "ephemeral", "Whether or not the message should be sent as an ephemeral message.")
+            ).queue()
         }
     }
 }
